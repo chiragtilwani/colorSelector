895258dfcd7d3ffe020692b27d42d1b06d8b9f9f
Chirag <chiragtilwani08@gmail.com>
2022-02-11 15:07:14 +0530

    style color slider

--- src/Palette.js
+++ src/Palette.js
@@ -3,23 +3,31 @@ import ColorBox from './ColorBox';
 import './Palette.css'
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Slider.css'
 
 class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state={level:500}
+        this.state = { level: 500 }
         this.handleChange = this.handleChange.bind(this)
     }
-    handleChange(newLevel){
-        this.setState({level:newLevel})
+    handleChange(newLevel) {
+        this.setState({ level: newLevel })
     }
     render() {
         return (
             <div className="palette">
-                <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.handleChange}/>
+                <div className="slider-container">
+                    <div className="slider-p">
+                        <p>Level : {this.state.level}</p>
+                    </div>
+                    <div className="slider">
+                    <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.handleChange} />
+                    </div>
+                </div>
                 {/* header comes here */}
                 <div className="palette-colors">
-                    {this.props.palette.colors[this.state.level].map(c=><ColorBox background={c.hex} name ={c.name} key={c.name}/>)}
+                    {this.props.palette.colors[this.state.level].map(c => <ColorBox background={c.hex} name={c.name} key={c.name} />)}
                 </div>
                 {/* footer comes here */}
             </div>
