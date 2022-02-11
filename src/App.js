424b263712729fddc10fd02495fde7f6a5483120
Chirag <chiragtilwani08@gmail.com>
2022-02-11 12:40:09 +0530

    add colorHelper to generate shades of colors

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import React,{Component} from 'react';
 import Palette from './Palette'
 import PaletteArray from './SeedColor'
+import {generatePalette} from './colorHelper'
 
 class App extends Component {
   render() {
+    console.log(generatePalette(PaletteArray[4]))
     return (
       <div className="App">
         <Palette {...PaletteArray[4]}/>
