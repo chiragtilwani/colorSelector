8db79ce454c3dbba848521b7382f4215a77cceb1
Chirag <chiragtilwani08@gmail.com>
2022-02-13 14:42:53 +0530

    add select format

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,50 @@
-import React,{ Component} from 'react';
+import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './NavBar.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+// Material UI imports
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+// import Button from '@mui/material/Button';
+// import Snackbar from '@mui/material/Snackbar';
+// import IconButton from '@mui/material/IconButton';
+// import CloseIcon from '@mui/icons-material/Close';
+
 class NavBar extends Component {
     render() {
         return (
-            <div className="slider-container">
-                <Link to='/' className="logo">Color<span className="selector">Selector</span></Link>
+            <header className="header">
+                <div className="slider-container">
+                    <Link to='/' className="logo">Color<span className="selector">Selector</span></Link>
                     <div className="slider-p">
                         <p>Level : {this.props.level}</p>
                     </div>
                     <div className="slider">
-                    <Slider defaultValue={this.props.level} min={100} max={900} step={100} onAfterChange={this.props.handleChange} />
+                        <Slider defaultValue={this.props.level} min={100} max={900} step={100} onAfterChange={this.props.handleChange} />
                     </div>
                 </div>
+                <div className="select-container">
+
+                            <InputLabel variant="standard" htmlFor="uncontrolled-native" className="label">
+                                Format
+                            </InputLabel>
+                            <NativeSelect
+                                defaultValue="hex"
+                                inputProps={{
+                                    name: 'Format',
+                                    id: 'uncontrolled-native',
+                                }}
+                                onChange={this.props.handleFormatChange}
+                            >
+                                <option value="hex">HEX - #ffffff</option>
+                                <option value="rgb">RGB - rgb(255,255,255)</option>
+                                <option value="rgba">RGBA - rgba(255,255,255,1.0)</option>
+                            </NativeSelect>
+                </div>
+            </header>
         )
     }
 }
--- src/Palette.js
+++ src/Palette.js
@@ -7,20 +7,26 @@ import NavBar from './NavBar';
 class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 }
+        this.state = { level: 500,format:"hex" }
         this.handleChange = this.handleChange.bind(this)
+        this.handleFormatChange = this.handleFormatChange.bind(this)
     }
     handleChange(newLevel) {
         this.setState({ level: newLevel })
     }
+    handleFormatChange(evt){
+        this.setState({format: evt.target.value})
+    }
     render() {
         return (
             <div className="palette">
-                <NavBar level={this.state.level} handleChange={this.handleChange}/>
-                {/* header comes here */}
+
+                <NavBar level={this.state.level} handleChange={this.handleChange} handleFormatChange={this.handleFormatChange}/>
+
                 <div className="palette-colors">
-                    {this.props.palette.colors[this.state.level].map(c => <ColorBox background={c.hex} name={c.name} key={c.name} />)}
+                    {this.props.palette.colors[this.state.level].map(c => <ColorBox background={c[this.state.format]} name={c.name} key={c.name} />)}
                 </div>
+                
                 {/* footer comes here */}
             </div>
         )
