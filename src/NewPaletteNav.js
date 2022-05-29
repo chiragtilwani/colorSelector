d02ab97d918437e80b96a1f6aa84f6c848d083c4
Chirag <chiragtilwani08@gmail.com>
2022-05-29 20:02:19 +0530

    add savePaletteForm

--- src/NewPaletteNav.js
+++ src/NewPaletteNav.js
@@ -1,15 +1,17 @@
 import * as React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import "./styles/NewPaletteForm.css";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { BiAddToQueue } from 'react-icons/bi';
+import SavePaletteForm from './SavePaletteForm'
 
 function NewPaletteNav(props) {
-    const {open,AppBar,handleDrawerOpen,navigate,savePalette,paletteName,handlePaletteNameChange,colorArray}=props
+    const { open, AppBar, handleDrawerOpen, navigate, savePalette, paletteName, handlePaletteNameChange, colorArray } = props
+
+
     return (<div>
         <CssBaseline />
         <AppBar position="fixed" open={open} color="default">
@@ -40,39 +42,7 @@ function NewPaletteNav(props) {
                         </Link>
                     </Typography>
                 </div>
-                <div className="btn-container">
-                    <button
-                        onClick={() => { navigate('/') }}
-                        className="custom-btn go-back-btn"
-                        style={{
-                            textDecoration: "none",
-                            fontWeight: "bold",
-                            paddingTop: "1rem",
-                        }}
-                    >
-                        Go Back
-                    </button>
-                    <ValidatorForm onSubmit={savePalette}>
-                        <TextValidator
-                            label="Palette Name"
-                            value={paletteName}
-                            onChange={handlePaletteNameChange}
-                            validators={["required", "isPaletteNameUnique"]}
-                            errorMessages={[
-                                "this field is required",
-                                "palette name must be unique",
-                            ]}
-                        />
-                        <button
-                            className={colorArray.length === 0 ? 'disableBtn' : "custom-btn save-palette-btn"}
-                            style={{ textDecoration: "none", fontWeight: "bold" }}
-                            type="submit"
-                            disabled={colorArray.length === 0 ? true : false}
-                        >
-                            Save Palette
-                        </button>
-                    </ValidatorForm>
-                </div>
+                <SavePaletteForm savePalette={savePalette} navigate={navigate} paletteName={paletteName} handlePaletteNameChange={handlePaletteNameChange} colorArray={colorArray}/>
             </Toolbar>
         </AppBar>
 
--- src/SavePaletteForm.js
+++ src/SavePaletteForm.js
@@ -1,8 +1,83 @@
-function SavePaletteForm(){
+import * as React from "react";
+import "./styles/NewPaletteForm.css";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import './styles/SavePaletteForm.css'
+
+function SavePaletteForm(props) {
+    const { savePalette, navigate, paletteName, handlePaletteNameChange, colorArray } = props
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
-        <div className="bg">
-            <div>save palette form</div>
-        </div>
+        <div >
+            <Dialog open={open} onClose={handleClose} className='dialog'>
+                <DialogTitle className='dialog-title'>Palette Name</DialogTitle>
+                <DialogContent className='dialog-content'>
+                    <DialogContentText className='dialog-content-text'>
+                        Please enter name for your palette. Make sure it's unique !
+                    </DialogContentText>
+
+                </DialogContent>
+                <ValidatorForm onSubmit={savePalette} >
+                    <TextValidator
+                        label="Palette Name"
+                        value={paletteName}
+                        onChange={handlePaletteNameChange}
+                        validators={["required", "isPaletteNameUnique"]}
+                        errorMessages={[
+                            "this field is required",
+                            "palette name must be unique",
+                        ]}
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        variant="standard"
+                        style={{margin:'0 1rem 0 2.7rem',width:'85%'}}
+                    />
+                    <DialogActions>
+                        <Button onClick={handleClose} className="dialog-btn">Cancel</Button>
+                        <Button type="submit" className="dialog-btn">Save</Button>
+                    </DialogActions>
+                </ValidatorForm>
+            </Dialog>
+
+            <div className="btn-container">
+                <button
+                    onClick={() => { navigate('/') }}
+                    className="custom-btn go-back-btn"
+                    style={{
+                        textDecoration: "none",
+                        fontWeight: "bold",
+                        paddingTop: "1rem",
+                    }}
+                >
+                    Go Back
+                </button>
+
+                <button
+                    className={colorArray.length === 0 ? 'disableBtn' : "custom-btn save-palette-btn"}
+                    style={{ textDecoration: "none", fontWeight: "bold" }}
+                    onClick={handleClickOpen}
+                    disabled={colorArray.length === 0 ? true : false}
+                >
+                    Save Palette
+                </button>
+
+            </div>
+        </div >
     )
 }
 
